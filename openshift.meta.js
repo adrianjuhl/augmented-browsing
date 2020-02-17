7d9ca6be7c67d1ca750a713be52918e1be58b5f9
Adrian Juhl <adrian.juhl@gmail.com>
2020-02-17 21:39:28 +1030

    switch updateURL and downloadURL to feature branch name.

--- openshift.meta.js
+++ openshift.meta.js
@@ -2,12 +2,12 @@
 // @name         Openshift Web Console augmentation
 // @author       Adrian Juhl
 // @namespace    https://github.com/adrianjuhl
-// @version      0.1.4
+// @version      0.1.5
 // @description  Adds a text field to the Pipelines page that filters the pipelines to those whose name contains the given text.
 // @match        https://rhos-console.services.adelaide.edu.au:8443/*
 // @match        https://rhosd-console.services.adelaide.edu.au:8443/*
 // @grant        none
-// @updateURL    https://github.com/adrianjuhl/augmented-browsing/raw/master/openshift.meta.js
-// @downloadURL  https://github.com/adrianjuhl/augmented-browsing/raw/master/openshift.user.js
+// @updateURL    https://github.com/adrianjuhl/augmented-browsing/raw/style-pipeline-elements/openshift.meta.js
+// @downloadURL  https://github.com/adrianjuhl/augmented-browsing/raw/style-pipeline-elements/openshift.user.js
 // @require      http://code.jquery.com/jquery-3.4.1.min.js
 // ==/UserScript==
--- openshift.user.js
+++ openshift.user.js
@@ -2,13 +2,13 @@
 // @name         Openshift Web Console augmentation
 // @author       Adrian Juhl
 // @namespace    https://github.com/adrianjuhl
-// @version      0.1.4
+// @version      0.1.5
 // @description  Adds a text field to the Pipelines page that filters the pipelines to those whose name contains the given text.
 // @match        https://rhos-console.services.adelaide.edu.au:8443/*
 // @match        https://rhosd-console.services.adelaide.edu.au:8443/*
 // @grant        none
-// @updateURL    https://github.com/adrianjuhl/augmented-browsing/raw/master/openshift.meta.js
-// @downloadURL  https://github.com/adrianjuhl/augmented-browsing/raw/master/openshift.user.js
+// @updateURL    https://github.com/adrianjuhl/augmented-browsing/raw/style-pipeline-elements/openshift.meta.js
+// @downloadURL  https://github.com/adrianjuhl/augmented-browsing/raw/style-pipeline-elements/openshift.user.js
 // @require      http://code.jquery.com/jquery-3.4.1.min.js
 // ==/UserScript==
 
@@ -19,6 +19,7 @@ jQuery(async function(){
   if(windowsLocationPathname.indexOf('pipelines') > -1) {
     await sleep(1000);
     addPipelinesNameFilterElements();
+    stylePipelineElements();
   }
 });
 
@@ -44,6 +45,7 @@ window.addEventListener('pushState', async function(e) {
   if(windowsLocationPathname.indexOf('pipelines') > -1) {
     await sleep(100);
     addPipelinesNameFilterElements();
+    stylePipelineElements();
   }
 });
 
